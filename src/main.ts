31d5bd22737f041fd69a04fbf9fb3d545bbcd22e
ederyc <edyepezc@ucsc.edu>
2024-11-04 12:23:47 -0800

    L16 Dialog Button implement

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,10 @@
-// todo
+// todo add button element that the user can click to see a message
+const button = document.createElement("button");
+button.textContent = "Click Me! Click Me!";
+document.body.appendChild(button);
+
+function displayMessage() {
+  alert("You look AMAZING today!");
+}
+
+button.addEventListener("click", displayMessage);
